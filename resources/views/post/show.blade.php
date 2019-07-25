04caed9d0eb89f8246e8db1c85f5989bb7cb6485
Ruli <rulisetiawan1989@gmail.com>
2019-07-25 08:37:10 +0700

    polimorphic laravel test

---
+++ resources/views/post/show.blade.php
@@ -0,0 +1,6 @@
+<h3>{{ $post->name }}</h3>
+<ul>
+    @foreach($post->comments as $comment)
+    <li>{{ $comment->body }}</li>
+    @endforeach
+</ul>
---
+++ resources/views/video/show.blade.php
@@ -0,0 +1,6 @@
+<h3>{{ $video->name }}</h3>
+<ul>
+    @foreach($video->comments as $comment)
+    <li>{{ $comment->body }}</li>
+    @endforeach
+</ul>
